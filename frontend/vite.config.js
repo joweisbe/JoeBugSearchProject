61962f2db772a45dcc0470ff7c3fcdc97881ecca
Joe Weisbeck <joweisbe@microsoft.com>
2025-03-17 10:02:49 -0700

    updating vite.config.js to remove changes I added earlier

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,8 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './',
-  build: {
-    outDir: 'dist',
-  },
+//  base: './',
+//  build: {
+//    outDir: 'dist',
+//  },
 })
