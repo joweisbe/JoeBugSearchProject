76a27ed4535ce867bba7470987be585bf60a0f51
Joe Weisbeck <joweisbe@microsoft.com>
2025-03-17 08:41:32 -0700

    Committing auto-updated files

--- backend/obj/Debug/net9.0/BugSearchBackend.AssemblyInfo.cs
+++ backend/obj/Debug/net9.0/BugSearchBackend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BugSearchBackend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fb1ec94d95e4f7fd2760e5da589c3370aeb53c9d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+99a365a07d51af8d1843fd8b9bd6dd75fb932c98")]
 [assembly: System.Reflection.AssemblyProductAttribute("BugSearchBackend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BugSearchBackend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
