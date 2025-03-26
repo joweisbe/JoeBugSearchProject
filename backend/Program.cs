4fcffb136483877f175003d94fdebd8122b6985e
Joe Weisbeck <joweisbe@microsoft.com>
2025-03-26 08:29:55 -0700

    ConfigureFunctionsWebApplication to Program.cs

--- backend/Program.cs
+++ backend/Program.cs
@@ -2,7 +2,8 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Azure.Functions.Worker.Configuration;
 
 var host = new HostBuilder()
-    .ConfigureFunctionsWorkerDefaults()  // Registers Azure Functions worker
+    //.ConfigureFunctionsWorkerDefaults()  // Registers Azure Functions worker
+    .ConfigureFunctionsWebApplication()
     .Build();
 
 host.Run();
